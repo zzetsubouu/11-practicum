d805b438264f13d362fdddb6bdc2680a0a1b1bcf
zzetsubouu <pyanix.mladshiy@gmail.com>
2024-11-12 21:47:52 +0200

    добавлена первая задача

---
+++ 1-task/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            SayBye();
+        }
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+        public static void SayBye()
+        {
+            Console.WriteLine("Bye");
+        }
+
+    }
+}
