89f1d4dcc12611c153c792aa6156ce1c050fb9c9
zzetsubouu <pyanix.mladshiy@gmail.com>
2024-11-12 23:27:45 +0200

    добавлено второе задание

---
+++ 2task/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = InputX("x");
+            int y = InputX("y");
+            Console.WriteLine($"сумма значений: {Sum(x, y)}");
+            Console.WriteLine($"разность значений: {Diff(x, y)}");
+            Console.WriteLine($"произведение значений: {Multipl(x, y)}");
+            Console.WriteLine($"частное значений: {Div(x, y):N3}");
+
+            Console.ReadKey();
+        }
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Diff(int x, int y)
+        {
+            return x - y;
+        }
+        public static int Multipl(int x, int y)
+        {
+            return x * y;
+        }
+        public static double Div(int x, int y)
+        {
+            return (double)x / y;
+        }
+        public static int InputX(string x)
+        {
+            Console.Write($"введите первое значение:");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+        public static int InputY(string y)
+        {
+            Console.Write($"введите первое значение: ");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
